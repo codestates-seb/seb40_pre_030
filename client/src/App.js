d3c5adf3971fc2f0502f439fbe6abdd8c7c13d47
2pandi <coder.2pandi@gmail.com>
2022-10-27 00:10:18 +0900

    :sparkles: feat: Set Main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import { GlobalStyle } from ".";
-import Header from "./components/Header";
-
-
+import Main from "./pages/Main";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App"></div>
-      <Header />
+      <div className="App">
+        <Main />
+      </div>
     </>
   );
 }
