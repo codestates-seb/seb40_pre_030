7a5cc6e552e1fcca23d327af69dd551267e4653f
2pandi <coder.2pandi@gmail.com>
2022-10-27 00:09:30 +0900

    :sparkles: feat: Complete Answer component

--- client/src/components/Answer/Answer.js
+++ client/src/components/Answer/Answer.js
@@ -1,22 +1,34 @@
 import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import Markdown from "../Markdown";
+import UserCard from "../UserCard";
+import Vote from "../Vote";
 
-const StyledAnswer = styled.li``;
+const StyledAnswer = styled.li`
+  border-bottom: lightgray solid 1px;
+  margin-top: 30px;
+  display: grid;
+  .answer-body {
+    grid-column: 2/3;
+  }
+  .UserCard {
+    grid-column: 2/3;
+    justify-self: flex-end;
+  }
+`;
 
 const Answer = () => {
   return (
     <StyledAnswer className="Answer">
-      <div className="answer-vote">
-        <FontAwesomeIcon icon={faCaretUp} />
-        <div>{"2"}</div>
-        <FontAwesomeIcon icon={faCaretDown} />
-      </div>
+      <Vote voteCount={1353} />
       <div className="answer-body">
         <Markdown />
       </div>
-      <div className="answer-user-info"></div>
+      <UserCard
+        answer
+        createdAt="Mar 6, 2011 at 12:43"
+        photoURL="https://www.gravatar.com/avatar/37d008229a56f1f8cb7a017644f9554d?s=64&d=identicon&r=PG"
+        displayName="Matt Price"
+      />
     </StyledAnswer>
   );
 };
