db9793b71bd7864f24be52db4ed09381a3da448d
2pandi <coder.2pandi@gmail.com>
2022-10-26 22:03:10 +0900

    :sparkles: feat: Add Vote component

---
+++ client/src/components/Vote.js
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const StyledVote = styled.div`
+  font-size: 1.7rem;
+  color: gray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .vote-icon {
+    font-size: 3.3rem;
+  }
+  .answer-body {
+    grid-column: 2/3;
+  }
+`;
+
+const Vote = ({ voteCount }) => {
+  return (
+    <StyledVote className="Vote">
+      <FontAwesomeIcon className="vote-icon" icon={faCaretUp} />
+      <div>{voteCount}</div>
+      <FontAwesomeIcon className="vote-icon" icon={faCaretDown} />
+    </StyledVote>
+  );
+};
+
+export default Vote;
