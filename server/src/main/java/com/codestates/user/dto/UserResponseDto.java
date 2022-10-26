63b1780efdc16a9a0f028abf8876eabd8461f477
J4mbo9 <chkfrdlek@naver.com>
2022-10-26 11:47:31 +0900

    getUserStatus 수정

--- server/src/main/java/com/codestates/user/dto/UserResponseDto.java
+++ server/src/main/java/com/codestates/user/dto/UserResponseDto.java
@@ -1,11 +1,14 @@
 package com.codestates.user.dto;
 
 import com.codestates.user.entity.User;
+import com.codestates.user.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 
 @Getter
+@Builder
 public class UserResponseDto {
 
     private long userId;
@@ -14,7 +17,7 @@ public class UserResponseDto {
     private String password;
     private User.UserStatus userStatus;
 
-    public UserResponseDto(User.UserStatus userStatus) {
-        this.userStatus = userStatus;
+    public String getUserStatus(){
+        return userStatus.getStatus();
     }
 }
