e315476a1a2d23f29cfbbe72a68685bd4bb29850
yujung-Jang <yjung.dev@gmail.com>
2022-10-26 11:06:40 +0900

    :sparkles: feat: Implement Board

---
+++ server/src/main/java/com/codestates/board/service/BoardService.java
@@ -0,0 +1,60 @@
+package com.codestates.board.service;
+
+import com.codestates.board.entity.Board;
+import com.codestates.board.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    // 질문 생성
+    public Board createBoard(Board board) {
+
+        return boardRepository.save(board);
+    }
+
+    // 질문 수정
+    public Board updateBoard(Board board) {
+
+        // 수정할 질문이 존재하는지 확인
+        Board findBoard = findVerifiedBoard(board.getBoardId());
+
+        Optional.ofNullable(board.getTitle()).ifPresent(title -> findBoard.setTitle(title));
+        Optional.ofNullable(board.getBody()).ifPresent(body -> findBoard.setBody(body));
+
+        return boardRepository.save(findBoard);
+    }
+
+    // 질문 전체 목록 출력
+    public Page<Board> findposts(int page, int size) {
+
+        return boardRepository.findAll(PageRequest.of(page, size, Sort.by("boardId").descending()));
+    }
+
+    // 질문 삭제
+    public void deleteBoard(long boardId) {
+
+        Board findBoard = findVerifiedBoard(boardId);
+        boardRepository.delete(findBoard);
+    }
+
+    // 질문이 존재하는지 검증
+    public Board findVerifiedBoard(long boardId) {
+
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+        Board findPost = optionalBoard.orElseThrow(() -> new RuntimeException("QUESTION_NOT_FOUND"));
+
+        return findPost;
+    }
+}
