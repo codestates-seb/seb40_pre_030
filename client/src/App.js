69945b9cd04a8c1583e9d8d712c8b29aaca6bbd1
2pandi <coder.2pandi@gmail.com>
2022-10-27 17:52:18 +0900

    :lipstick: style: Reset global style

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import { GlobalStyle } from ".";
 import Main from "./pages/Main";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <div className="App">
         <Main />
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
