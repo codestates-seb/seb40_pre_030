a4a3d866da9393b8fa81520476122ef5d44ff230
yujung-Jang <yjung.dev@gmail.com>
2022-10-27 14:00:56 +0900

    :sparkles: feat: 엔티티 간의 연관 관계 추가

--- server/src/main/java/com/codestates/board/entity/Board.java
+++ server/src/main/java/com/codestates/board/entity/Board.java
@@ -1,12 +1,13 @@
 package com.codestates.board.entity;
 
+import com.codestates.answer.entity.Answer;
+import com.codestates.user.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter @Setter
 @Entity
@@ -15,8 +16,15 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
-//    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
     private String title;
+
     private String body;
-//    private Long viewCount;
+
+    @OneToMany(mappedBy = "board")
+    private List<Answer> answer = new ArrayList<>();
 }
