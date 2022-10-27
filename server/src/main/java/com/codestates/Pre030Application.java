9bcc5006fe582205ec985cff0ed6cc118d9e31fd
mingu0429 <mingu0429@gmail.com>
2022-10-27 18:25:44 +0900

    ♻ refactor: @SpringBootApplication 어노테이션 수정

--- server/src/main/java/com/codestates/Pre030Application.java
+++ server/src/main/java/com/codestates/Pre030Application.java
@@ -2,12 +2,13 @@ package com.codestates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Pre030Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
