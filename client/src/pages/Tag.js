63f17f557c8547b40218a1d98fe0fc914d63ee10
headring <myhakiki@gmail.com>
2022-10-28 17:21:12 +0900

    :sparkles:feat: tag 페이지 적용

---
+++ client/src/pages/Tag.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar/Navbar";
+import Sidebar from "../components/Sidebar";
+
+const Tag = () => {
+  return (
+    <>
+      <Navbar />
+      <div>hi tag</div>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Tag;
