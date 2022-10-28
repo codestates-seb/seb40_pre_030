f4aa9e509ca53273d0f07ca468cd33c686a0baec
yujung-Jang <yjung.dev@gmail.com>
2022-10-28 12:53:06 +0900

    :loud_sound: feat: 로그인 인증 성공 및 실패에 따른 추가 처리

--- server/src/main/java/com/codestates/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/com/codestates/auth/filter/JwtAuthenticationFilter.java
@@ -12,8 +12,10 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -50,7 +52,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response,
                                             FilterChain chain,
-                                            Authentication authResult) {
+                                            Authentication authResult) throws ServletException, IOException {
 
         User user = (User) authResult.getPrincipal();
 
@@ -59,6 +61,8 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         response.setHeader("Authorization", "Bearer " + accessToken);
         response.setHeader("Refresh", refreshToken);
+
+        this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
     }
 
     private String delegateAccessToken(User user) {
