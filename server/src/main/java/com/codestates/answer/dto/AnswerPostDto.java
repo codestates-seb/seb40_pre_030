d84e3c357d78ebd440f2659313004d46feaee7da
mingu0429 <mingu0429@gmail.com>
2022-10-29 17:47:41 +0900

    ♻ refactor: Dto 주석처리

--- server/src/main/java/com/codestates/answer/dto/AnswerPostDto.java
+++ server/src/main/java/com/codestates/answer/dto/AnswerPostDto.java
@@ -1,22 +1,22 @@
-package com.codestates.answer.dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-
-@Getter
-@Setter
-public class AnswerPostDto {
-    // @Pattern(regexp = "/^.{15,}$/", message = "15자 이상 작성해야합니다.")
-    @NotBlank(message = "답변을 작성해주세요.")
-    private String answerBody;
-    private String photoURL;
-    private long boardId;
-
-    //@Positive
-    //private long userId;
-    //@Positive
-    //private long boardId;
-}
+//package com.codestates.answer.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.Positive;
+//
+//@Getter
+//@Setter
+//public class AnswerPostDto {
+//    // @Pattern(regexp = "/^.{15,}$/", message = "15자 이상 작성해야합니다.")
+//    @NotBlank(message = "답변을 작성해주세요.")
+//    private String answerBody;
+//    private String photoURL;
+//    private long boardId;
+//
+//    //@Positive
+//    //private long userId;
+//    //@Positive
+//    //private long boardId;
+//}
--- server/src/main/java/com/codestates/user/dto/UserResponseDto.java
+++ server/src/main/java/com/codestates/user/dto/UserResponseDto.java
@@ -1,26 +1,26 @@
-package com.codestates.user.dto;
-
-import lombok.Builder;
-import lombok.Getter;
-
-import java.time.LocalDateTime;
-
-@Getter
-@Builder
-public class UserResponseDto {
-
-    private long userId;
-    private String email;
-    private String nickName;
-    private String password;
-    private String photoURL;
-    private LocalDateTime createdAt;
-
-/*
-    private User.UserStatus userStatus;
-
-    public String getUserStatus(){
-        return userStatus.getStatus();
-    }
-*/
-}
+//package com.codestates.user.dto;
+//
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@Builder
+//public class UserResponseDto {
+//
+//    private long userId;
+//    private String email;
+//    private String nickName;
+//    private String password;
+//    private String photoURL;
+//    private LocalDateTime createdAt;
+//
+///*
+//    private User.UserStatus userStatus;
+//
+//    public String getUserStatus(){
+//        return userStatus.getStatus();
+//    }
+//*/
+//}
