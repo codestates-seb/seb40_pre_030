bd205e329166821b11664dbffb56f29e48f0d942
kjs0508 <kss0866@gmail.com>
2022-10-30 17:13:52 +0900

     :truck:rename:폴더 변경

---
+++ client/src/components/Answer/AnswerCreate.js
@@ -0,0 +1,43 @@
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+import styled from "styled-components";
+
+const Createform = styled.div`
+  width: 95%;
+  margin: auto;
+  float: right;
+  .AnswerCreatetitle {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    font-size: 25px;
+    font-weight: bold;
+  }
+  .Postwrap {
+    margin-top: 30px;
+    .Postbtn {
+      font-size: 15px;
+      background-color: #0a95ff;
+      border: none;
+      border-radius: 5px;
+      color: #fff;
+      margin-top: 15px;
+      padding: 15px;
+    }
+  }
+`;
+const AnswerCreate = () => {
+  return (
+    <>
+      <Createform>
+        <div className="AnswerCreatetitle">
+          <h1>Your answer</h1>
+        </div>
+        <Editor height="350px" initialEditType="markdown" initialValue="" />
+        <div className="Postwrap">
+          <button className="Postbtn">Post Your Answer</button>
+        </div>
+      </Createform>
+    </>
+  );
+};
+export default AnswerCreate;
