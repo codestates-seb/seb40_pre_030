3b99babcb7f2896d44ae7713e3a40f75723ddc81
2pandi <coder.2pandi@gmail.com>
2022-11-02 00:23:43 +0900

    :lipstick: design: Set font style to tag

--- client/src/components/tags/Tag.js
+++ client/src/components/tags/Tag.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 const StyledTag = styled.button`
   background-color: hsl(205, 46%, 92%);
   color: ${(props) => props.theme.tagBlueFont};
+  font-size: 0.75rem;
+  letter-spacing: 0.02em;
   border: none;
   border-radius: 0.2rem;
   padding: 0.3rem;
