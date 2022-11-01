ed14f1fdabc42eb3b05c1cf77e11de1a0873ee96
headring <myhakiki@gmail.com>
2022-11-01 17:09:55 +0900

    :sparkles:feaet: root에 recoil 적용 후 atom 폴더 및 파일 작성

--- client/src/App.js
+++ client/src/App.js
@@ -10,25 +10,28 @@ import LogOut from "./components/LogOut";
 import Askquetion from "./components/Askquetion";
 import Users from "./pages/Users";
 import QuestionsView from "./pages/QuestionView";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/*" element={<Main />} />
-          <Route path="/users/login" element={<Login />} />
-          <Route path="/users/signup" element={<Signup />} />
-          <Route path="/users/logout" element={<LogOut />} />
-          <Route path="/ask" element={<Askquetion />} />
-          <Route path="/question/:id" element={<QuestionsView />}></Route>
-          {/* 아래는 확인용. 나중에 지울 것 */}
-          <Route path="/users" element={<Users />}></Route>
-        </Routes>
-      </div>
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/*" element={<Main />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/users/signup" element={<Signup />} />
+            <Route path="/users/logout" element={<LogOut />} />
+            <Route path="/ask" element={<Askquetion />} />
+            <Route path="/question/:id" element={<QuestionsView />}></Route>
+            {/* 아래는 확인용. 나중에 지울 것 */}
+            <Route path="/users" element={<Users />}></Route>
+          </Routes>
+        </div>
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
 
