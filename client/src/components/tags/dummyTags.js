6bc11f9b22557c92c15d07d146ea3f4393321661
2pandi <coder.2pandi@gmail.com>
2022-11-01 16:55:24 +0900

    :recycle: refactor: Separate dummy tags

---
+++ client/src/components/tags/dummyTags.js
@@ -0,0 +1,42 @@
+export const DummyTags = [
+  {
+    id: 1,
+    tags: "javascript",
+    tagsbody:
+      "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
+    tagsnum: 2439957,
+    tagsask: [689, 4075],
+  },
+  {
+    id: 1,
+    tags: "javascript",
+    tagsbody:
+      "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
+    tagsnum: 2439957,
+    tagsask: [689, 4075],
+  },
+  {
+    id: 1,
+    tags: "javascript",
+    tagsbody:
+      "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
+    tagsnum: 2439957,
+    tagsask: [689, 4075],
+  },
+  {
+    id: 1,
+    tags: "javascript",
+    tagsbody:
+      "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
+    tagsnum: 2439957,
+    tagsask: [689, 4075],
+  },
+  {
+    id: 1,
+    tags: "javascript",
+    tagsbody:
+      "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
+    tagsnum: 2439957,
+    tagsask: [689, 4075],
+  },
+];
