ed0fd2cdf6f41ff84dafbbd0700a505cdb88efd9
2pandi <coder.2pandi@gmail.com>
2022-11-01 16:58:47 +0900

    :lipstick: style: Set style for main components

--- client/src/pages/Tags.js
+++ client/src/pages/Tags.js
@@ -2,7 +2,14 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
-import Tagsbody from "./Tagsbody";
+import Tagsbody from "../components/tags/Tagsbody";
+import { DummyTags } from "../components/tags/dummyTags";
+import Navbar from "../components/navbar/Navbar";
+
+const StyledDiv = styled.div`
+  display: grid;
+  grid-template-columns: auto 80%;
+`;
 
 const Tagcontent = styled.div`
   max-width: 80%;
@@ -33,93 +40,53 @@ const Tags = () => {
   const [searchText, setSearchText] = useState("");
   const handleChange = (e) => setSearchText(e.target.value);
 
-  const Dummy = [
-    {
-      id: 1,
-      tags: "javascript",
-      tagsbody:
-        "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
-      tagsnum: 2439957,
-      tagsask: [689, 4075],
-    },
-    {
-      id: 1,
-      tags: "javascript",
-      tagsbody:
-        "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
-      tagsnum: 2439957,
-      tagsask: [689, 4075],
-    },
-    {
-      id: 1,
-      tags: "javascript",
-      tagsbody:
-        "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
-      tagsnum: 2439957,
-      tagsask: [689, 4075],
-    },
-    {
-      id: 1,
-      tags: "javascript",
-      tagsbody:
-        "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
-      tagsnum: 2439957,
-      tagsask: [689, 4075],
-    },
-    {
-      id: 1,
-      tags: "javascript",
-      tagsbody:
-        "For questions about programming in ECMAScript (JavaScript/JS) and its different dialects/implementations (except for ActionScript). Keep in mind that JavaScript is NOT the same as Java! Include all labels that are relevant to your question; e.g., [node.js], [jQuery], [JSON], [ReactJS], [angular], [ember.js], [vue.js], [typescript], [svelte], etc. ",
-      tagsnum: 2439957,
-      tagsask: [689, 4075],
-    },
-  ];
-
   return (
-    <Tagcontent>
-      <div className="TagsTitle">Tags</div>
-      <div className="TagsBody">
-        <div>
-          A tag is a keyword or label that categorizes your question with other,
-          similar questions. Using the right tags makes it easier for others to
-          find and answer your question.
-        </div>
-
-        <a href="https://stackoverflow.com/tags/synonyms" className="taglink">
-          Show all tag synonyms
-        </a>
-        <div className="filtertags">
-          <form>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-            <input
-              className="search-bar"
-              placeholder="Search..."
-              value={searchText}
-              onChange={handleChange}
-            />
-          </form>
+    <StyledDiv>
+      <Navbar />
+      <Tagcontent className="">
+        <div className="TagsTitle">Tags</div>
+        <div className="TagsBody">
           <div>
-            <button className="questions-tab">Popular</button>
-            <button className="questions-tab">Name</button>
-            <button className="questions-tab">New</button>
+            A tag is a keyword or label that categorizes your question with
+            other, similar questions. Using the right tags makes it easier for
+            others to find and answer your question.
+          </div>
+
+          <a href="https://stackoverflow.com/tags/synonyms" className="taglink">
+            Show all tag synonyms
+          </a>
+          <div className="filtertags">
+            <form>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              <input
+                className="search-bar"
+                placeholder="Search..."
+                value={searchText}
+                onChange={handleChange}
+              />
+            </form>
+            <div>
+              <button className="questions-tab">Popular</button>
+              <button className="questions-tab">Name</button>
+              <button className="questions-tab">New</button>
+            </div>
+          </div>
+          <div className="Tagscard">
+            {DummyTags.map((el) => {
+              return (
+                <Tagsbody
+                  key={el.id}
+                  tags={el.tags}
+                  tagsbody={el.tagsbody}
+                  tagsnum={el.tagsnum}
+                  tagsask={el.tagsask}
+                />
+              );
+            })}
           </div>
         </div>
-        <div className="Tagscard">
-          {Dummy.map((el) => {
-            return (
-              <Tagsbody
-                key={el.id}
-                tags={el.tags}
-                tagsbody={el.tagsbody}
-                tagsnum={el.tagsnum}
-                tagsask={el.tagsask}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </Tagcontent>
+      </Tagcontent>
+    </StyledDiv>
   );
 };
 
