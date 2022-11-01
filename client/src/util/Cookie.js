33be1c7be26c1ae573a4eaafbc341770dc05ebb6
headring <myhakiki@gmail.com>
2022-11-01 17:37:02 +0900

    :sparkles:feat: 쿠키 설정 함수 생성

---
+++ client/src/util/Cookie.js
@@ -0,0 +1,12 @@
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+export function setRefreshTokenToCookie(refresh_token) {
+  cookies.set("refresh_token", refresh_token, { sameSite: "strict" });
+}
+
+export function logout() {
+  console.log("localStorage set logout!");
+  window.localStorage.setItem("logout", Date.now());
+  cookies.remove("refresh_token");
+}
