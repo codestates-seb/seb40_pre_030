3cce638de6a96d019888f93300d0a243752bfe72
2pandi <coder.2pandi@gmail.com>
2022-11-02 00:24:28 +0900

    :lipstick: design: Set tagsbody style

--- client/src/components/tags/Tagsbody.js
+++ client/src/components/tags/Tagsbody.js
@@ -2,16 +2,17 @@ import styled from "styled-components";
 import Tag from "./Tag";
 
 const TagsBox = styled.div`
-  width: 15rem;
-  height: 10rem;
-  margin: 1rem;
-  border: 1px solid black;
-  .tagsnum {
-    display: flex;
-    flex-wrap: wrap;
+  width: 10rem;
+  min-width: 8rem;
+  height: 3rem;
+  border: 1px solid lightgray;
+  border-radius: 3px;
+  padding: 12px;
 
-    width: 5rem;
-    flex-direction: column;
+  .tagsnum {
+    color: ${(props) => props.theme.grayFont};
+    margin-top: 1rem;
+    font-size: 0.8rem;
   }
 `;
 
@@ -19,7 +20,9 @@ const Tagsbody = ({ data }) => {
   return (
     <TagsBox>
       <Tag value={data.name} />
-      <div className="tagsnum">{data.count} questions</div>
+      <div className="tagsnum">
+        {data.count.toLocaleString("ko-KR")} questions
+      </div>
     </TagsBox>
   );
 };
