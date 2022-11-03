0b3e14a2e1e104850b76a79c471e410185e10542
yujung-Jang <yjung.dev@gmail.com>
2022-11-03 14:19:26 +0900

    :sparkles: feat: 로그인 성공 시 응답 데이터 추가

--- server/src/main/java/com/codestates/auth/handler/UserAuthenticationSuccessHandler.java
+++ server/src/main/java/com/codestates/auth/handler/UserAuthenticationSuccessHandler.java
@@ -36,9 +36,10 @@ public class UserAuthenticationSuccessHandler implements AuthenticationSuccessHa
         User user = userRepository.findByEmail(authentication.getName())
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
 
-        SuccessUserInfo successUserInfo = new SuccessUserInfo(HttpStatus.ACCEPTED.value(), user.getEmail(), user.getPhotoURL());
+        SuccessUserInfo successUserInfo = new SuccessUserInfo(HttpStatus.ACCEPTED.value(), user.getEmail(), user.getNickName(), user.getPhotoURL());
         String responseInfo = new Gson().toJson(successUserInfo);
 
+        response.setCharacterEncoding("utf-8");
         response.setStatus(HttpStatus.ACCEPTED.value());
         response.getWriter().write(responseInfo);
     }
