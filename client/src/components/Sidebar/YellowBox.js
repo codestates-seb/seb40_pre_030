3cccdcfe50516cb7e051a67e8c4a981bb15f2a1a
2pandi <coder.2pandi@gmail.com>
2022-11-04 01:59:37 +0900

    :lipstick: design: Change icons into sprites

--- client/src/components/Sidebar/YellowBox.js
+++ client/src/components/Sidebar/YellowBox.js
@@ -1,6 +1,5 @@
-import { faCommentAlt, faPen } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import { ReactComponent as Pencil } from "../../assets/pencil.svg";
 
 export const StyledYellowBox = styled.div`
   background-color: #fef6df;
@@ -19,6 +18,7 @@ export const StyledYellowBox = styled.div`
 
   .icon-wrapper {
     height: 100%;
+    margin: 0 0.4rem;
   }
 
   .icon {
@@ -46,47 +46,58 @@ export const StyledYellowBox = styled.div`
 const listContents = {
   faPen: [
     {
-      icon: faPen,
+      icon: "faPen",
       href: "https://stackoverflow.blog/2022/10/24/how-hardware-and-software-can-maximize-your-flow-states/?cb=1&_ga=2.206072915.1741655448.1666525029-1930885377.1656908536",
       content:
         "He helped build .NET and VS Code — Now’s he working on Web3 (Ep. 499)",
     },
     {
-      icon: faPen,
+      icon: "faPen",
       href: "https://stackoverflow.blog/2022/10/25/a-flight-simulator-for-developers-to-practice-real-world-challenges-and-surprises-ep-500/?cb=1&_ga=2.234070366.1741655448.1666525029-1930885377.1656908536",
       content: "How hardware and software can maximize your flow states",
     },
   ],
   faCommentAlt: [
     {
-      icon: faCommentAlt,
+      icon: "faCommentAlt",
       href: "https://meta.stackexchange.com/questions/383022/the-2022-community-a-thon-has-begun?cb=1",
       content: "The 2022 Community-a-thon has begun!",
     },
     {
-      icon: faCommentAlt,
+      icon: "faCommentAlt",
       href: "https://meta.stackexchange.com/questions/383026/mobile-app-infrastructure-being-decommissioned?cb=1",
       content: "Staging Ground Workflow: Canned Comments",
     },
     {
-      icon: faCommentAlt,
+      icon: "stack",
       href: "https://meta.stackoverflow.com/questions/420897/staging-ground-workflow-canned-comments?cb=1",
       content: "The [script] tag is being burninated",
     },
     {
-      icon: faCommentAlt,
+      icon: "stack",
       href: "https://meta.stackoverflow.com/questions/406928/the-script-tag-is-being-burninated?cb=1",
       content: "Ask Wizard Test Graduation",
     },
   ],
 };
 
+const Icon = styled.div`
+  height: 14px;
+  width: 14px;
+  background-image: ${(props) => props.theme.iconSprites};
+  background-position: ${(props) =>
+    props.icon === "comment" ? "0 -6120px" : "0 -6156px"};
+  background-size: 16px 7038px;
+`;
+
 export const ListContent = ({ listContent, id }) => {
   return (
     <li className="Sidecontent" key={id}>
       <div className="list-item">
         <span className="icon-wrapper">
-          <FontAwesomeIcon className="icon" icon={listContent.icon} />
+          {listContent.icon === "faPen" && <Pencil width="14" height="14" />}
+          {listContent.icon === "faCommentAlt" && <Icon icon="comment" />}
+          {listContent.icon === "stack" && <Icon icon="stack" />}
         </span>
         <a href={listContent.href}>{listContent.content}</a>
       </div>
