b7c3b7cc80137b8ed9d446b308a5e5da2c91bf2d
2pandi <coder.2pandi@gmail.com>
2022-11-04 15:15:17 +0900

    :bug: fix: Add anonymous avatar img

--- client/src/components/Answer/AnswersContainer.js
+++ client/src/components/Answer/AnswersContainer.js
@@ -163,7 +163,11 @@ const AnswersContainer = () => {
                         ).toLocaleString("ko-KR", {
                           timeZone: "UTC",
                         })}
-                        photoURL={userInfo.photoURL}
+                        photoURL={
+                          userInfo && userInfo.photoURL
+                            ? userInfo.photoURL
+                            : "https://www.gravatar.com/avatar/3b37be7c3ac00a1237fe8d4252fd4540.jpg?size=240&d=https%3A%2F%2Fwww.artstation.com%2Fassets%2Fdefault_avatar.jpg"
+                        }
                         displayName={datas.nickName}
                       />
                     </div>
