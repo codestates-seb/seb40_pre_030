91297cbb68b49455003eda283b8c224200b78bb9
Yujung Jang <yjung.dev@gmail.com>
2022-11-07 16:18:21 +0900

    🐛 fix: CORS 설정 수정

--- server/src/main/java/com/codestates/config/WebConfig.java
+++ server/src/main/java/com/codestates/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")
                 .allowedOriginPatterns("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization", "Refresh")
                 .allowCredentials(true).maxAge(3600);
