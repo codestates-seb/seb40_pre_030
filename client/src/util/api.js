a6bf4214b3858d38498d5b12b8699262450333f2
2pandi <coder.2pandi@gmail.com>
2023-01-02 15:42:37 +0900

    :sparkles: feat: update BASE_URL

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://54.180.95.121:8080/";
+export const BASE_URL = "http://43.201.14.122:8080/";
 
 export const STACK_EXCHANGE_URL = "https://api.stackexchange.com/2.3/";
 
